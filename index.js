e7773c0043212b2fb2c14e65dee3f4bcce6939b9
Frederik Bolding <frederik.bolding@gmail.com>
2019-11-02 11:25:48 +0100

    Changed Google Auth method

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const fs = require('fs')
-const readline = require('readline')
 const { google } = require('googleapis')
 const TelegramBot = require('node-telegram-bot-api')
 const pointsBot = require('./pointsbot.js')
@@ -8,7 +7,6 @@ let privateRooms = {}
 
 // If modifying these scopes, delete credentials.json.
 const SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
-const TOKEN_PATH = 'credentials.json'
 
 // Load client secrets from a local file.
 fs.readFile('client_secret.json', (err, content) => {
@@ -33,49 +31,21 @@ fs.readFile('./privateRooms.json', 'utf8', function(err, data) {
  * @param {function} callback The callback to call with the authorized client.
  */
 function authorize(credentials, callback) {
-  const { client_secret, client_id, redirect_uris } = credentials.installed
-  const oAuth2Client = new google.auth.OAuth2(
-    client_id,
-    client_secret,
-    redirect_uris[0]
+  let jwtClient = new google.auth.JWT(
+    credentials.client_email,
+    null,
+    credentials.private_key,
+    SCOPES
   )
-
-  // Check if we have previously stored a token.
-  fs.readFile(TOKEN_PATH, (err, token) => {
-    if (err) return getNewToken(oAuth2Client, callback)
-    oAuth2Client.setCredentials(JSON.parse(token))
-    callback(oAuth2Client)
-  })
-}
-
-/**
- * Get and store new token after prompting for user authorization, and then
- * execute the given callback with the authorized OAuth2 client.
- * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
- * @param {getEventsCallback} callback The callback for the authorized client.
- */
-function getNewToken(oAuth2Client, callback) {
-  const authUrl = oAuth2Client.generateAuthUrl({
-    access_type: 'offline',
-    scope: SCOPES,
-  })
-  console.log('Authorize this app by visiting this url:', authUrl)
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  })
-  rl.question('Enter the code from that page here: ', code => {
-    rl.close()
-    oAuth2Client.getToken(code, (err, token) => {
-      if (err) return callback(err)
-      oAuth2Client.setCredentials(token)
-      // Store the token to disk for later program executions
-      fs.writeFile(TOKEN_PATH, JSON.stringify(token), err => {
-        if (err) console.error(err)
-        console.log('Token stored to', TOKEN_PATH)
-      })
-      callback(oAuth2Client)
-    })
+  //authenticate request
+  jwtClient.authorize(function(err, tokens) {
+    if (err) {
+      console.log(err)
+      return
+    } else {
+      console.log('Successfully connected!')
+      callback(jwtClient)
+    }
   })
 }
 
